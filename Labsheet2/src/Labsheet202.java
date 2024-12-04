b95af870107f765f8b23b632b7aae21049f7c892
nitipromhrNTeam <niti29350@gmail.com>
2024-12-04 11:23:46 +0700

    Labsheet2

---
+++ Labsheet2/src/Labsheet202.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Labsheet202 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int newNumber = Integer.MIN_VALUE;
+		int oldNumber;
+		while(true) {
+			System.out.println("Input number : ");
+			oldNumber = Scanner.nextInt();
+			if (oldNumber < newNumber) {
+				break;
+			}
+			newNumber = oldNumber;
+		}
+		System.out.println("BYE BYE");
+		scanner.close();
+	}
+
+}
---
+++ Labsheet2/src/Labsheet203.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class Labsheet203 {
+
+	public static void main(String[] args) {
+			System.out.println("Input Weight:");
+
+	}
+
+}
