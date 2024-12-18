4c108dfc9f7dbfe5d948e7471c5c4b60cfd52f96
nitipromhrNTeam <niti29350@gmail.com>
2024-12-18 08:37:15 +0700

    Labsheet03

---
+++ Labsheet03/src/Lab303.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Lab303 {
+
+	public static void main(String[] args) {
+		System.out.print("Input a sentence : ");
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+
+}
