af35bab5e7c0a397ea7331badfe0560c48f29c56
nitipromhrNTeam <niti29350@gmail.com>
2024-12-25 11:06:48 +0700

    Lab507

---
+++ Labsheet05/src/Lab507.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class Lab507 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for (i=0;i<0;i++) {
+		int number = Integer.parseInt(JOptionPane.showInputDialog(null ,"Input number 1 : "+(i+1));
+		num += number;
+		}
+	}
+
+}
