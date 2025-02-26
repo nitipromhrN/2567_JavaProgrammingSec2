6b1d4147b5b074609b603afee5536e7389df5131
nitipromhrNTeam <niti29350@gmail.com>
2025-02-26 11:19:38 +0700

    Labsheet11

---
+++ Labsheet11/src/ATMChecking.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ATMChecking {
+	private int money;
+	
+	public ATMChecking(String accountNumber, String password, int money) {
+		this.accountNumber = accountNumber;
+		this.password = password;
+		this.money = money;
+	}
+	
+}
